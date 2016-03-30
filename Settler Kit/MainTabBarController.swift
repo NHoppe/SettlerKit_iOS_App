b38ed1555c9167d8b77c9313a90767d000d56d7d
NHoppe <nhoppe@gmail.com>
2016-03-30 16:55:53 -0700

    Fixed layout.

---
+++ Settler Kit/MainTabBarController.swift
@@ -0,0 +1,31 @@
+//
+//  MainTabBarController.swift
+//  Settler Kit
+//
+//  Created by N Hoppe on 2016-03-30.
+//  Copyright © 2016 Natan Hoppe. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
+        return UIInterfaceOrientationMask.Portrait
+    }
+    
+    override func shouldAutorotate() -> Bool {
+        return true
+    }
+}
